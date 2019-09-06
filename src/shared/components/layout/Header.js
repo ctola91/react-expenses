cb3619a854c377aa9be7e249757e4b36959660b9
cmtp <christian.tola@hotmail.com>
2019-09-06 14:59:48 -0400

    add header styles and routes to pages

--- src/shared/components/layout/Header.js
+++ src/shared/components/layout/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import logo from '../../../assets/images/logo.svg';
 
-const logoStyle = {
-  maxWidth: '320px',
+const burgerStyle = {
+  borderStyle: 'none',
+  outlineStyle: 'none',
+  background: '#ffffff',
 };
 
 const Header = props => {
@@ -13,12 +16,46 @@ const Header = props => {
     url = 'http://localhost:3000',
   } = props;
   return (
-    <header className="app-header">
-      <a href={url}>
-        <img src={logo} alt="expenses" style={logoStyle} />
-      </a>
-      <h1 className="App-title">{title}</h1>
-    </header>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href={url}>
+          <img src={logo} width="112" height="28" alt={title} />
+        </a>
+
+        <button
+          type="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          style={burgerStyle}
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </button>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">Home</Link>
+          <Link to="/about" className="navbar-item">About</Link>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <button className="button is-primary" type="button">
+                <strong>Sign up</strong>
+              </button>
+              <button type="button" className="button is-light">
+                Log in
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
